5b7cc1a43f45ff4a570dd34db326dfdefb2ca92c
SelvinAnibalToledo <144059429+SelvinAnibalToledo@users.noreply.github.com>
2023-10-09 19:27:35 -0700

    Update settings.py

--- moviesApp/settings.py
+++ moviesApp/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'moviesApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "django_bootstrap",
+        "USER": "ubuntu",
+        "PASSWORD": "thisissomeseucrepassword",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
