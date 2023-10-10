dce115bda1cbc8c5454d23804c964f1e8b6ba82e
SelvinAnibalToledo <Selvin3901@gmail.com>
2023-10-10 04:10:45 +0000

    se configura el django-envior para seguridad

--- moviesApp/settings.py
+++ moviesApp/settings.py
@@ -11,20 +11,22 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gn0*l(7s!na_^us5%$kpy63y*5&wo63cil1va+t7j3ej0$dy)#'
-
+SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = env('DEBUG')
 ALLOWED_HOSTS = ['prograweb.ddns.net', 'localhost', '127.0.0.1','selvinweb.ddns.net']
 
 
@@ -75,22 +77,17 @@ WSGI_APPLICATION = 'moviesApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "django_bootstrap",
-        "USER": "ubuntu",
-        "PASSWORD": "thisissomeseucrepassword",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "ENGINE": env('ENGINE'),
+        "NAME": env('NAME'),
+        "USER": env('USER'),
+        "PASSWORD": env('PASSWORD'),
+        "HOST": env('HOST'),
+        "PORT": env('PORT'),
     }
 }
+
 
 
 # Password validation
