10ff64a090fa5227d2491c2de07da34004250570
Fernando Cano <jose.cano193@tectijuana.edu.mx>
2023-10-19 02:29:52 +0000

    Se agrego tailwind css

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Movie 
 
 # Create your views here.
+def index(request):
+    movies = Movie.objects.all()
+    print(movies)
+    return render(request, 'index.html', {"movies":movies})
